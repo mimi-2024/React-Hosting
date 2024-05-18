eb9b775fc903375ce43617bd3ec40dc280ef05f5
mimi-2024 <mimi.barik@gmail.com>
2024-05-18 23:12:47 +0530

    Vite-project

---
+++ react_vite/component/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <div>
+      <nav>
+        <ul>
+            <li>
+                home
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+      </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
